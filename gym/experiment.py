4abfa74f19af586107d11c37d1a1b2e1428991d1
Thomas Reeves <tjtommy5000@gmail.com>
2023-11-19 14:06:54 -0800

    debug

--- gym/experiment.py
+++ gym/experiment.py
@@ -121,7 +121,8 @@ def experiment(
     # used to reweight sampling so we sample according to timesteps instead of trajectories
     p_sample = traj_lens[sorted_inds] / sum(traj_lens[sorted_inds])
 
-    def get_pretrain_batch(traj, si, max_len):     
+    def get_pretrain_batch(traj_num, si, max_len):  
+        traj = trajectories[int(sorted_inds[traj_num])]   
         s, a, r, d, rtg, timesteps, mask = [], [], [], [], [], [], []   
         s.append(traj['observations'][si:si + max_len].reshape(1, -1, state_dim))
         a.append(traj['actions'][si:si + max_len].reshape(1, -1, act_dim))
