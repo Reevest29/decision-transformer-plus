2dfd05104c21cc439b714850483c4872e92ce25d
Thomas Reeves <tjtommy5000@gmail.com>
2023-12-01 00:05:17 -0800

    debug

--- gym/decision_transformer/training/pretrainer.py
+++ gym/decision_transformer/training/pretrainer.py
@@ -128,6 +128,40 @@ class PreTrainer:
         return loss.detach().cpu().item()
     
     def pretrain_step(self):
+        states, actions, rewards, dones, rtg, timesteps, attention_mask = self.get_batch(self.batch_size)
+        action_target = torch.clone(actions)
+        state_target = torch.clone(states)
+        reward_target = torch.clone(rewards)
+
+        state_preds, action_preds, reward_preds = self.model.forward(
+            states, actions, rewards, rtg[:,:-1], timesteps, attention_mask=attention_mask,
+        )
+
+        act_dim = action_preds.shape[2]
+        action_preds = action_preds.reshape(-1, act_dim)[attention_mask.reshape(-1) > 0]
+        action_target = action_target.reshape(-1, act_dim)[attention_mask.reshape(-1) > 0]
+
+        state_dim = state_preds.shape[2]
+        state_preds = state_preds.reshape(-1, state_dim)[attention_mask.reshape(-1) > 0]
+        state_target = state_target.reshape(-1, state_dim)[attention_mask.reshape(-1) > 0]
+
+        reward_dim = reward_preds.shape[2]
+        reward_preds = reward_preds.reshape(-1, reward_dim)[attention_mask.reshape(-1) > 0]
+        reward_target = reward_target.reshape(-1, reward_dim)[attention_mask.reshape(-1) > 0]
+
+        loss = ((state_preds - state_target) **2) + ((reward_preds - reward_target) **2)
+
+        self.optimizer.zero_grad()
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_(self.model.parameters(), .25)
+        self.optimizer.step()
+
+        with torch.no_grad():
+            self.diagnostics['training/action_error'] = torch.mean((action_preds-action_target)**2).detach().cpu().item()
+
+        return loss.detach().cpu().item()
+    
+    def z_pretrain_step(self):
         # traj = self.get_trajectory(0)
         
 
